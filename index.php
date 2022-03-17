236f82e21312277b0ddf29afc8370a20f4eff987
David Mecalco Alejo <dmecalcoa1200@alumno.ipn.mx>
2022-03-17 11:02:20 -0600

    Iniciamos el repositorio para el sistema

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Bracket">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:url" content="http://themepixels.me/bracket">
+    <meta property="og:title" content="Bracket">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta property="og:image" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/bracket/img/bracket-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+    <title>ABYSSAL::Acceso</title>
+
+    <link href="public/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="public/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="public/select2/css/select2.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="public/css/bracket.css">
+  </head>
+
+  <body>
+
+    <div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+
+      <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white rounded shadow-base">
+        <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span>ABYSSAL CONSULTACY<span class="tx-normal">]</span></div>
+        <div class="tx-center mg-b-40">Sistema de Certificados y Diplomas</div>
+
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Correo electronico">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-info btn-block">Ingresar</button>
+      </div><!-- login-wrapper -->
+    </div><!-- d-flex -->
+
+    <script src="public/jquery/jquery.js"></script>
+    <script src="public/popper.js/popper.js"></script>
+    <script src="public/bootstrap/bootstrap.js"></script>
+    <script src="public/select2/js/select2.min.js"></script>
+    <script>
+      $(function(){
+        'use strict';
+
+        $('.select2').select2({
+          minimumResultsForSearch: Infinity
+        });
+      });
+    </script>
+
+  </body>
+</html>
